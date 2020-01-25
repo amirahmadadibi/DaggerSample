a71e9401e1d5d89a3dee9625681a4e7e7ba2d98a
AmirahmadAdibi <amirahmadadibi@gmail.com>
2020-01-25 16:06:31 +0330

    adding dependencies

---
+++ app/src/main/java/net/codeinreal/amriahmadadibi/projects/samples/daggersmaple/MainActivity.kt
@@ -0,0 +1,12 @@
+package net.codeinreal.amriahmadadibi.projects.samples.daggersmaple
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
